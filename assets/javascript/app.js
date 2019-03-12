aa309c52ea409479bbeb6ef0a3d1910d8734a66e
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-03-11 23:50:27 -0600

    more

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
-//background array
-// var bgArray = ['../images/arches.jpg', '../images/milky-way.jpg', '../images/road.jpg'],
-//     selectBG = bgArray[Math.floor(Math.random() * bgArray.length)];
-// $("body").css("background", "url(' + selectBG + ')");
+// randomize background array. (not working for some reason)
+// var bgArray = ["../images/arches.jpg", "../images/milky-way.jpg", "../images/road.jpg"];
+// var selectBG = bgArray[Math.floor(Math.random() * bgArray.length)];
+// $("body").css("background", "url('" + selectBG + "')");
 
 
 // Timer
@@ -23,177 +23,170 @@ var timer = setInterval(function () {
 // make array with questions and answers objects
 var quizQuestions = [{
         question: "Beaverdam wash is the lowest point in Utah. What is the elevation? ",
-        answer: {
-            a: "680 ft",
-            b: "2000 ft",
-            c: "2940 ft",
-            d: "1450 ft"
-        },
-        correct: "b"
+        answer: [
+            " 680 ft",
+            " 2000 ft",
+            " 2940 ft",
+            " 1450 ft"
+        ],
+        correct: "2000 ft"
     },
     {
         question: "What is the highest point in the state of Utah? ",
-        answer: {
-            a: "Bald Mtn",
-            b: "Lone Peak",
-            c: "Mt Timpanogos",
-            d: "Kings Peak"
-        },
-        correct: "d"
+        answer: [
+            " Bald Mtn",
+            " Lone Peak",
+            " Mt Timpanogos",
+            " Kings Peak"
+        ],
+        correct: "Kings Peak"
     },
     {
         question: "What is the state bird of Utah? ",
-        answer: {
-            a: "Perrigrine Falcon",
-            b: "Hummingbird",
-            c: "Quail",
-            d: "California Gull"
-        },
-        correct: "d"
+        answer: [
+            " Perrigrine Falcon",
+            " Hummingbird",
+            " Quail",
+            " California Gull"
+        ],
+        correct: "California Gull"
     },
     {
         question: "Levan, Utah is: ",
-        answer: {
-            a: "In the middle of the state",
-            b: "Navel spelled backward",
-            c: "Named by Brigham Young",
-            d: "All of the above"
-        },
-        correct: "d"
+        answer: [
+            " In the middle of the state",
+            " Navel spelled backward",
+            " Named by Brigham Young",
+            " All of the above"
+        ],
+        correct: "All of the above"
     },
     {
         question: "The world's largest natural-rock span standing 278 feet wide and 309 feet high is called? ",
-        answer: {
-            a: "Delicate Arch",
-            b: "Devil's Garden",
-            c: "Rainbow Bridge",
-            d: "Landscape Arch"
-        },
-        correct: "c"
+        answer: [
+            " Delicate Arch",
+            " Devil's Garden",
+            " Rainbow Bridge",
+            " Landscape Arch"
+        ],
+        correct: "Rainbow Bridge"
     },
     {
         question: "What is the deepest point of the Great Salt Lake? ",
-        answer: {
-            a: "34 ft",
-            b: "62 ft",
-            c: "13 ft",
-            d: "28 ft"
-        },
-        correct: "a"
+        answer: [
+            " 34 ft",
+            " 62 ft",
+            " 13 ft",
+            " 28 ft"
+        ],
+        correct: "34 ft"
     },
     {
         question: "Utah's state symbol symbolizes thrift and industry. What is it? ",
-        answer: {
-            a: "Sewing Machine",
-            b: "Semi-Truck",
-            c: "Beehive",
-            d: "Seagull"
-        },
-        correct: "c"
+        answer: [
+            " Sewing Machine",
+            " Semi-Truck",
+            " Beehive",
+            " Seagull"
+        ],
+
+        correct: "Beehive"
     },
     {
         question: "Utah is the ___ largest state in the U.S.? ",
-        answer: {
-            a: "11th",
-            b: "32nd",
-            c: "6th",
-            d: "19th"
-        },
-        correct: "a"
+        answer: [
+            " 11th",
+            " 32nd",
+            " 6th",
+            " 19th"
+        ],
+        correct: "11th"
     },
     {
         question: "The largest employer in Utah is? ",
-        answer: {
-            a: "Adobe",
-            b: "LDS Church",
-            c: "Rio Tinto",
-            d: "United States Goverment"
-        },
-        correct: "d"
+        answer: [
+            " Adobe",
+            " LDS Church",
+            " Rio Tinto",
+            " United States Goverment"
+        ],
+        correct: "United States Goverment"
     },
     {
         question: "If you boiled down 1 quart of water from the Great Salt Lake, how much salt would remain? ",
-        answer: {
-            a: "4 tablespoons",
-            b: "1/2 cup",
-            c: "1/4 cup",
-            d: "1 cup"
-        },
-        correct: "b"
+        answer: [
+            " 4 tablespoons",
+            " 1/2 cup",
+            " 1/4 cup",
+            " 1 cup"
+        ],
+        correct: "1/2 cup"
     },
     {
         question: "Utah is the ____ driest state in the U.S.? ",
-        answer: {
-            a: "2nd",
-            b: "1st",
-            c: "5th",
-            d: "8th"
-        },
-        correct: "a"
+        answer: [
+            " 2nd",
+            " 1st",
+            " 5th",
+            " 8th"
+        ],
+        correct: "2nd"
     },
     {
         question: "Which one of these is not a Utah National Park? ",
-        answer: {
-            a: "Canyonlands",
-            b: "Capitol Reef",
-            c: "Escalante",
-            d: "Bryce Canyon"
-        },
-        correct: "c"
+        answer: [
+            " Canyonlands",
+            " Capitol Reef",
+            " Escalante",
+            " Bryce Canyon"
+        ],
+        correct: "Escalante"
     },
     {
         question: "What was the last major river to be discovered in the contiguous United States?",
-        answer: {
-            a: "The San Juan River",
-            b: "The Escalante River",
-            c: "The Virgin River",
-            d: "The Green river"
-        },
-        correct: "b"
+        answer: [
+            " The San Juan River",
+            " The Escalante River",
+            " The Virgin River",
+            " The Green river"
+        ],
+        correct: "The Escalante River"
     },
 
 ]
 
 
-//random pick questions 
-var questionTracker = [];
+// random pick questions 
+// post questions and answers to radio buttons on html page
+// store answers in variable
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unselectedAnswers = 0;
 for (i = 0; i < 6; i++) {
     var randQuestions = Math.floor(Math.random() * quizQuestions.length);
     console.log(quizQuestions[randQuestions].question);
     console.log(quizQuestions[randQuestions].answer);
     console.log(quizQuestions[randQuestions].correct);
     $("#the-questions").append("<p>" + quizQuestions[randQuestions].question + "</p>");
-    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='option' value=''>" + quizQuestions[randQuestions].answer.a + "</label>");
-    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='option' value=''>" + quizQuestions[randQuestions].answer.b + "</label>");
-    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='option' value=''>" + quizQuestions[randQuestions].answer.c + "</label>");
-    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='option' value=''>" + quizQuestions[randQuestions].answer.d + "</label>");
+    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='" + quizQuestions[randQuestions].question + "' value='" + quizQuestions[randQuestions].answer[0] + "'>" + quizQuestions[randQuestions].answer[0] + "</label>");
+    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='" + quizQuestions[randQuestions].question + "' value='" + quizQuestions[randQuestions].answer[1] + "'>" + quizQuestions[randQuestions].answer[1] + "</label>");
+    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='" + quizQuestions[randQuestions].question + "' value='" + quizQuestions[randQuestions].answer[2] + "'>" + quizQuestions[randQuestions].answer[2] + "</label>");
+    $("#the-questions").append("<label class='radio-inline'><input type='radio' name='" + quizQuestions[randQuestions].question + "' value='" + quizQuestions[randQuestions].answer[3] + "'>" + quizQuestions[randQuestions].answer[3] + "</label>");
 
 
-
-    // quizQuestions[randQuestions].splice(0, 1);
-
+    quizQuestions.splice(randQuestions, 1);
 };
 
+// create Done button at bottom, after questions
 
+// in Done button, function (if/else); also works for timing out
 
+// clear page & do following logic:
 
+// if (radio button checked value === found question's correct answer) -> increment correctAnswer
+// else if -> unselected -> increment unselectedAnswer
+// else -> increment incorrectAnswer
 
-
-
-
-// post questions and answers to radio buttons on html page
-
-// 
-
-// store answers in variable 
-
-
-//done button
-
-
-
-// answer page
-
-
+// answer page with results
 
 // refresh page and background image with "play again" button
